8c46e6a5b87930cfedd7d994756066f63b9d0239
Mukarram Asad <76425569+Muhammad-Mukarram-Asad@users.noreply.github.com>
2023-03-20 16:10:28 +0500

    Add files via upload

---
+++ App.js
@@ -0,0 +1,58 @@
+// import logo from "./logo.svg";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+import { Route, Routes, Link } from "react-router-dom";
+//  import Container from "react-bootstrap/Container";
+//  import Nav from "react-bootstrap/Nav";
+//  import Navbar from "react-bootstrap/Navbar";
+import Home from "./routing/Home";
+import About from "./routing/About";
+import Product from "./routing/Product";
+
+function App() {
+  return (
+    <div className="App">
+      <nav>
+        <ul>
+        <img src={"https://hysabkytab.com/wp-content/uploads/2022/05/white-logo.png"}
+            alt="HK Logo" />
+          <li> <Link to="/">HOME</Link></li>
+          <li> <Link to="/about">ABOUT US</Link></li>
+          <li> <Link to="/product">PRODUCTS</Link></li>
+          <li> <Link to="/resources">RESOURCES</Link></li>
+          <li> <Link to="/news">NEWS& MEDIA</Link></li>
+          <li> <Link to="/help">HELP</Link></li>
+          <li> <Link to="/contact">CONTACT US</Link></li>
+
+
+        </ul>
+      </nav>
+      {/* <Navbar bg="dark" variant="dark">
+        <Container>
+          <Navbar.Brand href="#home">
+            <img src={"https://hysabkytab.com/wp-content/uploads/2022/05/white-logo.png"}
+            alt="HK Logo" />
+          </Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="/">Home</Nav.Link>
+            <Nav.Link href="/about">About Us</Nav.Link>
+            <Nav.Link href="#pricing">Products</Nav.Link>
+            <Nav.Link href="#pricing">Resources</Nav.Link>
+            <Nav.Link href="#pricing">NEWS & MEDIA</Nav.Link>
+
+          </Nav>
+        </Container>
+      </Navbar> */}
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/product" element={<Product />} />
+
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
